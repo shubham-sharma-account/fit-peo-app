eafa1688f2ac9011f1996b613ac3c15e9995965f
Shubham Sharma <shubham.7.10.1999@gmail.com>
2025-05-29 23:59:33 +0530

    Activity chart done

--- src/ActivityBar.js
+++ src/ActivityBar.js
@@ -1,14 +1,26 @@
 import React from "react";
 
-const ActivityBar = ({data}) => {
-    console.log("h1, h2, h3, h4  ", data.h4); 
-    
+const ActivityBar = ({ data }) => {
+  console.log("h1, h2, h3, h4  ", data.h4);
+
   return (
     <div className="flex justify-between items-center w-12">
-      <div className={`w-1 bg-blue-200 h-${data.h1} rounded-full`}></div>
-      <div className={`w-1 bg-blue-400 h-${data.h2} rounded-full`}></div>
-      <div className={`w-1 bg-blue-300 h-${data.h3} rounded-full`}></div>
-      <div className={`w-1 bg-blue-500 h-${data.h4} rounded-full`}></div>
+      <div
+        className="w-1 bg-blue-200 rounded-full"
+        style={{ height: `${data.h1}px` }}
+      ></div>
+      <div
+        className="w-1 bg-blue-400 rounded-full"
+        style={{ height: `${data.h2}px` }}
+      ></div>
+      <div
+        className="w-1 bg-blue-300 rounded-full"
+        style={{ height: `${data.h3}px` }}
+      ></div>
+      <div
+        className="w-1 bg-blue-500 rounded-full"
+        style={{ height: `${data.h4}px` }}
+      ></div>
     </div>
   );
 };
